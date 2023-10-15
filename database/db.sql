0f9c609548a42a74a2161b0b3a0a60e5eead6661
jeslintay <jeslin.tay.2020@sis.smu.edu.sg>
2023-10-15 10:36:19 +0800

    Updated db

--- database/db.sql
+++ database/db.sql
@@ -3,13 +3,14 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+DROP SCHEMA sbrp;
 CREATE DATABASE IF NOT EXISTS sbrp DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE sbrp;
 
-CREATE TABLE IF NOT EXISTS `access_rights`(
-    Access_right_no int NOT NULL,
-    role varchar(20) NOT NULL,
-    CONSTRAINT access_rights_pk PRIMARY KEY (Access_right_no)
+CREATE TABLE IF NOT EXISTS Access_Control(
+    Access_ID int NOT NULL,
+    Access_Control_Name varchar(20) NOT NULL,
+    CONSTRAINT access_rights_pk PRIMARY KEY (Access_ID)
 );
 
 CREATE TABLE IF NOT EXISTS Staff (
@@ -21,7 +22,7 @@ CREATE TABLE IF NOT EXISTS Staff (
     Email varchar(50) NOT NULL,
     Access_Rights int NOT NULL,
     CONSTRAINT staff_pk PRIMARY KEY (Staff_ID),
-    CONSTRAINT staff_fk FOREIGN KEY (Access_Rights) REFERENCES access_rights(Access_right_no) 
+    CONSTRAINT staff_fk FOREIGN KEY (Access_Rights) REFERENCES Access_Control(Access_ID) 
 );
 
 CREATE TABLE IF NOT EXISTS role (
@@ -30,13 +31,13 @@ CREATE TABLE IF NOT EXISTS role (
     CONSTRAINT role_pkey PRIMARY KEY (role_name)
 );
 
-CREATE TABLE IF NOT EXISTS `skills`(
+CREATE TABLE IF NOT EXISTS skills(
     Skill_Name varchar(50) NOT NULL,
     Skill_Descr varchar(200) NOT NULL,
     CONSTRAINT skills_pk PRIMARY KEY (Skill_Name)
 );
 
-CREATE TABLE IF NOT EXISTS `Role_Skill`(
+CREATE TABLE IF NOT EXISTS Role_Skill(
     Role_Name varchar(20),
     Skill_Name varchar(50),
     CONSTRAINT Role_skill_pk PRIMARY KEY (Role_Name, Skill_Name),
