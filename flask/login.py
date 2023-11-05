4c0edc89481bfb1f91e1037d8bd2f347f108505c
aaronwky <aaron.wong.2021@scis.smu.edu.sg>
2023-11-05 16:16:12 +0800

    changed 200 to true

--- flask/login.py
+++ flask/login.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # if __name__ == '__main__':
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://' + \
-                                        'root:root' + \
+                                        'root:' + \
                                         '@localhost:3306/sbrp'
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                             'pool_recycle': 280}
--- flask/roles.py
+++ flask/roles.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # if __name__ == '__main__':
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://' + \
-                                        'root:root' + \
+                                        'root:' + \
                                         '@localhost:3306/sbrp'
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                             'pool_recycle': 280}
@@ -125,7 +125,7 @@ def create_role_listing():
             "message": "Incorrect JSON object provided."
         }),500
     
-    if validate_role_listing(data) == 200:
+    if validate_role_listing(data) == True:
         Listings = []
         for skill in data['skills_required']:
             print(skill)
@@ -168,9 +168,11 @@ def validate_role_listing(data):
             "message": "Deadline cannot be before today."
         }), 500
     
-    return jsonify({
-        "message": "OK"
-    }), 200
+    # return jsonify({
+    #     "message": "OK"
+    # }), 200
+
+    return True
 
 
 @app.route("/apply_role", methods=['POST'])
