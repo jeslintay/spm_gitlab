406061b780df1f8136ebf38fd1f2618850252eff
natworking <115059335+natworking@users.noreply.github.com>
2023-11-05 16:46:19 +0800

    unittest remove exception

--- flask/unit_tests.py
+++ flask/unit_tests.py
@@ -35,13 +35,11 @@ class TestListing(unittest.TestCase):
     def test_negative_deadline(self):
         l1 = Listing(role_name='Junior Engineer', role_descr='not senior engineer', skills_required='coding', role_deadline='2020-12-31')
 
-        try:
-            response = validate_role_listing(l1)
-            self.assertEqual(response.status_code, 500)  # Check the HTTP status code
-            data = response.get_json()  # Get the JSON data from the response
-            self.assertEqual(data["message"], "Deadline cannot be before today")  # Check the "message" in the JSON response
-        except Exception as e:
-            self.fail(f"Unexpected exception: {e}")
+        response = validate_role_listing(l1)
+        self.assertEqual(response.status_code, 500)  # Check the HTTP status code
+        data = response.get_json()  # Get the JSON data from the response
+        self.assertEqual(data["message"], "Deadline cannot be before today")  # Check the "message" in the JSON response
+        
 
     
     def test_negative_empty_input(self):
